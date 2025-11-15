c1b1f963c668222a1cb74b7179eca6645aa9f610
Harsh <userharsh911@gmail.com>
2025-11-15 21:42:29 +0530

    sixth commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ app.use("/api/message",messageRouter)
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
-    app.get("/:path(*)", (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
 }
