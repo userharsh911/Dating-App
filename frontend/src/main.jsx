502753e8a0657ef677cd7a7c4a98f0d811bd6050
Harsh <userharsh911@gmail.com>
2025-11-15 20:58:33 +0530

    sixth commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {createBrowserRouter, RouterProvider, createRoutesFromElements, Route} from "react-router-dom";
-import Home from '../pages/home.jsx';
+import Home from '../pages/Home.jsx';
 import Matches from '../pages/Matches.jsx';
 import Profile from '../pages/Profile.jsx';
 import Setting from '../pages/Setting.jsx';
